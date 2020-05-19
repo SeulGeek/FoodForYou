0ede03c37e8c685efd015e6d37ebc51c1597bd6e
SeulGeek <seulgi.choi.mobile@gmail.com>
2020-05-19 23:19:21 +0900

    [#4] [식단 목록] 메인 카테고리 리사이클러뷰로 작성
    - 가져온 API response를 리사이클러뷰로 연동시키기

--- app/src/main/java/com/example/foodforyou/MainActivity.java
+++ app/src/main/java/com/example/foodforyou/MainActivity.java
@@ -1,56 +1,86 @@
 package com.example.foodforyou;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodforyou.model.MainCategory;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mMainRecipeCategory;
-    private String data;
+    private RecyclerView recyclerView;
+    private MainCategoryAdapter adapter;
+    private ArrayList<String> mainCategoryTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button inquiryButton = findViewById(R.id.button_inquiry);
-        mMainRecipeCategory = findViewById(R.id.tv_main_recipe_category);
+        init();
 
+        getMainCategoryTitleResponse();
+    }
+
+    private void init() {
+        recyclerView = findViewById(R.id.recycler_view_main_category);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new MainCategoryAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void getMainCategoryTitleResponse() {
         // Get Api response
-        inquiryButton.setOnClickListener(new View.OnClickListener() {
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
+            public void run() {
+                getMainCategoryResponse();
+                runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        data = getXmlData();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mMainRecipeCategory.setText(data);
-                            }
-                        });
+                        for (int i = 0; i< mainCategoryTitle.size(); i++) {
+                            // 각 List의 값들을 MainCategory 객체에 set 해줍니다
+                            MainCategory mainCategoryData = new MainCategory();
+                            mainCategoryData.setDietSeName(mainCategoryTitle.get(i));
+
+                            // 각 값들이 들어간 data를 adapter에 추가합니다.
+                            adapter.addItem(mainCategoryData);
+                        }
+
+                        // adapter의 값이 변경되었다는 것을 알려줍니다.
+                        adapter.notifyDataSetChanged();
                     }
-                }).start();
+                });
             }
-        });
+        }).start();
 
     }
 
-    private String getXmlData() {
+
+    private void getMainCategoryResponse() {
+        mainCategoryTitle = new ArrayList<>();
         String apiKey = ""; //TODO: set your api key
         String apiUrl = "http://api.nongsaro.go.kr/service/recomendDiet/mainCategoryList?apiKey=" + apiKey;
-        StringBuffer strBuffer = new StringBuffer();
 
         try{
             URL url = new URL(apiUrl);
@@ -63,32 +93,19 @@ public class MainActivity extends AppCompatActivity {
             int eventType = xpp.getEventType();
             while(eventType != XmlPullParser.END_DOCUMENT) {
                 switch (eventType) {
-                    case XmlPullParser.START_DOCUMENT:
-                        strBuffer.append("parsing start...\n\n");
-                        break;
                     case XmlPullParser.START_TAG:
                         if (xpp.getName().equals("item")) { // fist research result
 
                         } else if (xpp.getName().equals("dietSeName")) {
                             xpp.next();
-                            strBuffer.append(xpp.getText() + "\n");
+                            mainCategoryTitle.add(xpp.getText());
                         }
                         break;
-                    case XmlPullParser.TEXT:
-                        break;
-                    case XmlPullParser.END_DOCUMENT:
-                        if (xpp.getName().equals("item")) {
-                            strBuffer.append("\n"); // first research close, change line
-                        }
                 }
                 eventType= xpp.next();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        strBuffer.append("parsing end...\n");
-        return strBuffer.toString();
     }
-
 }
