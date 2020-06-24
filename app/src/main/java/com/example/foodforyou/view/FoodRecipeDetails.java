66a61898cdaa88e96a67529a9e9e7487d99cb0d3
SeulGeek <seulgi.choi.mobile@gmail.com>
2020-06-24 23:04:46 +0900

    [#17] [식단 소개] 유저가 선택한 음식의 상세 페이지 작성
    ( API 연동 )
    
    - 식단 소개 불러오기
    - 식단별 조리법 불러오기

---
+++ app/src/main/java/com/example/foodforyou/view/FoodRecipeDetails.java
@@ -0,0 +1,14 @@
+package com.example.foodforyou.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodRecipeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_recipe_details);
+    }
+}
